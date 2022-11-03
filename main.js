ccf64adf19ec0c08e35c2cf6e0bac966482fcf3b
constantine30 <banciu.constantin@gmail.com>
2022-11-03 15:25:33 +0000

    tests complete, all working

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 /* 
-👉 Write your kata here!
+Complete the function that accepts two integer arrays of equal length.
+Adds the value of each member in one array to the corresponding member in the other.
+Squares the sum of the two values.
+Returns the average of those sums rounded up.
+
+Example
+    [2,2] [2,2] => 16 (because 16 + 16 divided by 2 equals 16)
+
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
-
 export function firstTest(arr1, arr2){
-    console.log("hello")
-
-    // declare an empty array
-    // loop the first array
-    // loop the second array
-    // add the values of the same indices and push to new array
-
-    // loop through the new array and square each item
-
-    // add the values together 
-    // divide them by the length of the array
-    // return the value
-
-    let returnArr = []
-
-    for (let i = 0; i < arr1.length; i++){
-        returnArr.push(arr1[i] + arr2[i])
-    }
-    for (let i = 0; i < returnArr.length; i++){
-        returnArr[i] = (returnArr[i] * returnArr[i])
-    }
-    let result = 0
-    for (let i = 0; i < returnArr.length; i++){
-        result += returnArr[i]
-    }
-    result /= returnArr.length
-
-
-    console.log(result)
-    return result
+    // Your code here!
 }
--- main.test.js
+++ main.test.js
@@ -1,34 +1,14 @@
-//👉 Write your tests below here:
-//import {test} from '@jest/globals';
 import {firstTest} from './main.js';
 
-
-
 // test('check is working', () => {
-//     expect(firstTest([2,2,2], [2,2,2])).toBe(16);     //4, 16, 36 = 56/3 == 18.667365792356972
+//     expect(firstTest([2,2,2], [2,2,2])).toBe(16);
 // });
-
-
-
 
 test.each([
     {arr1: [2,2,2], arr2: [2,2,2], expected: 16},
-    {arr1: [5,9,4,8], arr2: [3,7,8,4], expected: 152},  
+    {arr1: [5,9,4,8], arr2: [3,7,8,4], expected: 152},
+    {arr1: [4,8,0,2,3], arr2: [5,1,2,7,6], expected: 66},
+    {arr1: [2,3,4,6,4,3], arr2: [2,6,1,1,5,7], expected: 59},
+    {arr1: [9,9,9,9,9,9,9], arr2: [11,22,33,44,55,66,77], expected: 3293},
 ])('Calculate the average of the square of the values', ({arr1, arr2, expected}) => {
     expect(firstTest(arr1, arr2)).toBe(expected)})
-
-
-// 
-
-// test('adds 1 + 2 to equal 3', () => {
-//     expect(sum(1, 2)).toBe(3);
-//   });
-
-// test.each([
-//     {word: "CODE", expected:7},
-//     {word: "ABCD", expected:9},
-//     {word: "ZELDA", expected:15},
-//     {word: "LIONS", expected:5},
-//     {word: "ECHO", expected:9}
-// ])('calculate value of word $word', ({word, expected}) => {
-// expect(calculateScrabbleScore(word)).toBe(expected)});
