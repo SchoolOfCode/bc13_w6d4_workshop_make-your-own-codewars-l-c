32220c368f288979b5217909313b2fc94ce25f09
L-E-W-1-5 <lewiswootton88@hotmail.com>
2022-11-03 15:04:31 +0000

    started creating a function with testing

--- main.js
+++ main.js
@@ -4,6 +4,35 @@
 
 //👉 Write the function your CodeWarriors will start with below here:
 
-export function firstTest(){
+export function firstTest(arr1, arr2){
     console.log("hello")
+
+    // declare an empty array
+    // loop the first array
+    // loop the second array
+    // add the values of the same indices and push to new array
+
+    // loop through the new array and square each item
+
+    // add the values together 
+    // divide them by the length of the array
+    // return the value
+
+    let returnArr = []
+
+    for (let i = 0; i < arr1.length; i++){
+        returnArr.push(arr1[i] + arr2[i])
+    }
+    for (let i = 0; i < returnArr.length; i++){
+        returnArr[i] = (returnArr[i] * returnArr[i])
+    }
+    let result = 0
+    for (let i = 0; i < returnArr.length; i++){
+        result += returnArr[i]
+    }
+    result /= returnArr.length
+
+
+    console.log(result)
+    return result
 }
--- main.test.js
+++ main.test.js
@@ -1,9 +1,34 @@
 //👉 Write your tests below here:
-import {test} from '@jest/globals';
+//import {test} from '@jest/globals';
 import {firstTest} from './main.js';
 
 
 
-test('check is working', () => {
-    firstTest()
-})
+// test('check is working', () => {
+//     expect(firstTest([2,2,2], [2,2,2])).toBe(16);     //4, 16, 36 = 56/3 == 18.667365792356972
+// });
+
+
+
+
+test.each([
+    {arr1: [2,2,2], arr2: [2,2,2], expected: 16},
+    {arr1: [5,9,4,8], arr2: [3,7,8,4], expected: 152},  
+])('Calculate the average of the square of the values', ({arr1, arr2, expected}) => {
+    expect(firstTest(arr1, arr2)).toBe(expected)})
+
+
+// 
+
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });
+
+// test.each([
+//     {word: "CODE", expected:7},
+//     {word: "ABCD", expected:9},
+//     {word: "ZELDA", expected:15},
+//     {word: "LIONS", expected:5},
+//     {word: "ECHO", expected:9}
+// ])('calculate value of word $word', ({word, expected}) => {
+// expect(calculateScrabbleScore(word)).toBe(expected)});
